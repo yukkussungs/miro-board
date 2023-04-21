5313a2bf1b182c357c26764b4a797851bf6ec77a
ssungs <sws7420@gmail.com>
2023-04-21 22:33:57 +0900

    set up clerk & convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://informed-condor-33.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
+ 
+export default authMiddleware({
+  // Allow signed out users to access the specified routes:
+  // publicRoutes: ['/anyone-can-visit-this-route'],
+  // Prevent the specified routes from accessing
+  // authentication information:
+  // ignoredRoutes: ['/no-auth-in-this-route'],
+});
+ 
+export const config = {
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/(api|trpc)(.*)"
+  ]
+};
