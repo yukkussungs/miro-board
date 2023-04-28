e3241ef35201bdbc6a5a70723bf1ef1e0fe38024
ssungs <sws7420@gmail.com>
2023-04-28 21:38:40 +0900

    favorit & search bug fix and fix db query on convex

--- convex/board.ts
+++ convex/board.ts
@@ -51,6 +51,17 @@ export const remove = mutation({
       if (!identity) {
         throw new Error("Unauthorized");
       }
+
+      const userId = identity.subject;
+
+      const existingFavorite = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) => q.eq("userId", userId).eq("boardId", args.id))
+        .unique();
+
+      if (existingFavorite) {
+        await ctx.db.delete(existingFavorite._id);
+      }
     
       await ctx.db.delete(args.id);
     },
@@ -102,8 +113,8 @@ export const remove = mutation({
   
       const existingFavorite = await ctx.db
         .query("userFavorites")
-        .withIndex("by_user_board_org", (q) => 
-          q.eq("userId", userId).eq("boardId", board._id).eq("orgId", args.orgId)
+        .withIndex("by_user_board", (q) => 
+          q.eq("userId", userId).eq("boardId", board._id)
         )
         .unique();
   
