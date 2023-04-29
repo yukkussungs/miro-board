d42ebbb10f2895f860dbd9e6626feea9ccede5b9
ssungs <sws7420@gmail.com>
2023-04-29 22:58:23 +0900

    liveblocks add

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -6,3 +6,9 @@ export const Participants = () => {
     );
   };
   
+  Participants.Skeleton = function ParticipantsSkeleton() {
+    return (
+      <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
+    );
+  };
+  
