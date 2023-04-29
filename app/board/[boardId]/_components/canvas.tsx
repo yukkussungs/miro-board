ecb78446784aa58d6cdb683398de3b2e3712d7dc
ssungs <sws7420@gmail.com>
2023-04-30 00:53:03 +0900

    room auth setting

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,6 +3,7 @@
 import { Info } from "./info";
 import { Toolbar } from "./toolbar";
 import { Participants } from "./participants";
+import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
@@ -11,6 +12,10 @@ interface CanvasProps {
 export const Canvas = ({
   boardId,
 }: CanvasProps) => {
+  const info = useSelf((me) => me.info);
+
+  console.log(info)
+  
   return (
     <main
       className="h-full w-full relative bg-neutral-100 touch-none"
